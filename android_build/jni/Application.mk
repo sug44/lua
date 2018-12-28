c6c93e5cf7cabd87e3944366e3c97b2c81272565
Vinicius Jarina <viniciusjarina@gmail.com>
2018-12-28 01:01:12 -0200

    * Removed android make file
    * Use ndk-build directly instead.

--- android_build/jni/Application.mk
+++ android_build/jni/Application.mk
@@ -1,4 +1,5 @@
 APP_MODULES := lua53
 APP_PLATFORM := android-18
 APP_OPTIM   := release
-APP_ABI := armeabi armeabi-v7a x86
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+NDK_TOOLCHAIN_VERSION := 4.9
