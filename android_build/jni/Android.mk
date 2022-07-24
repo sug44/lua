75f97884708cab6b7739333634fff184a53520ed
Vinicius Jarina <viniciusjarina@gmail.com>
2022-07-24 09:35:58 -0700

    Fixed build on Android
    ##[error]ld(0,0): Error : undefined symbol: stpcpy
    ld : error : undefined symbol: stpcpy [D:\a\1\s\build\Android\KeraLua.net6.0-android.csproj]
      >>> referenced by lstrlib.c:1171 (jni/../../src\lstrlib.c:1171)
      >>>               ./obj/local/armeabi-v7a/objs/lua54/__/__/src/lstrlib.o:(str_format)
      >>> did you mean: strcpy

--- android_build/jni/Android.mk
+++ android_build/jni/Android.mk
@@ -8,6 +8,7 @@ LOCAL_SRC_FILES :=  \
  ../../src/ldblib.c ../../src/ldebug.c ../../src/ldo.c ../../src/ldump.c ../../src/lfunc.c ../../src/lgc.c ../../src/linit.c ../../src/liolib.c \
  ../../src/llex.c ../../src/lmathlib.c ../../src/lmem.c ../../src/loadlib.c ../../src/lobject.c \
  ../../src/lopcodes.c ../../src/loslib.c ../../src/lparser.c ../../src/lstate.c ../../src/lstring.c ../../src/lstrlib.c ../../src/ltable.c \
- ../../src/ltablib.c ../../src/ltm.c ../../src/lundump.c ../../src/lutf8lib.c ../../src/lvm.c ../../src/lzio.c
+ ../../src/ltablib.c ../../src/ltm.c ../../src/lundump.c ../../src/lutf8lib.c ../../src/lvm.c ../../src/lzio.c \
+ ../../src/android_strpcpy.c
 
 include $(BUILD_SHARED_LIBRARY)
